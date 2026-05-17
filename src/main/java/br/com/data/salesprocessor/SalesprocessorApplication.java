6d9cd482de7a05bef5f096cb3aea6740f5f0c08e
aled-dev <aledadmpe@gmail.com>
2026-05-17 18:59:46 -0300

    chore: configura projeto, Docker e perfil dev

---
+++ src/main/java/br/com/data/salesprocessor/SalesprocessorApplication.java
@@ -0,0 +1,13 @@
+package br.com.data.salesprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesprocessorApplication.class, args);
+	}
+
+}
